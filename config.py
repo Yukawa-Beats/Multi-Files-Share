15c05ea2e93c1d974db6a2f28ce98b8be9ddc959
Yukawa Beats <89127713+Yukawa-Beats@users.noreply.github.com>
2021-09-01 23:05:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from logging.handlers import RotatingFileHandler
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-#Your API ID from @MT_MyTelegramOrg_Bot
+#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
 
-#Your API Hash from @MT_MyTelegramOrg_Bot
+#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
